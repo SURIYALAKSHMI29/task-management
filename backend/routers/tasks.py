470f0e656b0e93c3309c8901898fe1dca4fe0c2f
SuriyaLakshmi29 <suriyalakshmi413@gmail.com>
2025-10-23 22:16:15 +0530

    Add task history updation logic

--- backend/routers/tasks.py
+++ backend/routers/tasks.py
@@ -1,13 +1,15 @@
-from datetime import datetime
+import calendar
+from datetime import date, timedelta
 from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session, func, select
+from sqlalchemy.orm import joinedload
+from sqlmodel import Session, select
 
-from backend.database import get_session
+from backend.database import engine, get_session
 from backend.helpers.auth.auth_utils import get_current_user, verify_current_user
 from backend.helpers.enums import RecurrenceType, TaskPriority, TaskStatus
-from backend.models import RecurringTask, Task, User
+from backend.models import RecurringTask, Task, TaskHistory
 from backend.routers.users import get_user_by_email
 from backend.schemas import TaskIn, TaskOut
 
@@ -55,6 +57,84 @@ def check_pinned_tasks(user_id: int, session: Session):
     return True
 
 
+def scheduled_task_updates():
+    with Session(engine) as session:
+        stmt = (
+            select(Task)
+            .join(RecurringTask, Task.id == RecurringTask.task_id, isouter=True)
+            .where(
+                ((Task.deadline.is_not(None)) & (Task.deadline <= date.today()))
+                | (
+                    (RecurringTask.repetitive_type.is_not(None))
+                    & (
+                        (RecurringTask.repeat_until.is_(None))
+                        | (RecurringTask.repeat_until >= date.today())
+                    )
+                ),
+                Task.status == TaskStatus.PENDING,
+            )
+            .options(joinedload(Task.recurring_task))
+        )
+
+        tasks = session.exec(stmt).all()
+        print(tasks)
+        for task in tasks:
+            print("\nUpdating for ", task)
+            print(task.recurring_task, task.title)
+            updateTaskHistory(task, TaskStatus.PENDING, session)
+
+
+# creates appropriate task history based on its recurring type
+def updateTaskHistory(task: Task, status: TaskStatus, session: Session):
+    if task.deadline is None and task.recurring_task is None:
+        return
+    start = task.deadline
+    end = task.deadline
+    today = date.today()
+
+    # finding appropriate start and end dates
+    if task.recurring_task is not None:
+        recurring_task = session.get(RecurringTask, task.recurring_task.id)
+        if recurring_task.repetitive_type == RecurrenceType.WEEKLY:
+            nearest_monday = date.today() - timedelta(days=today.weekday())
+            start = nearest_monday
+            nearest_sunday = date.today() + timedelta(
+                days=(6 - date.today().weekday()) % 7
+            )
+            end = nearest_sunday
+        elif recurring_task.repetitive_type == RecurrenceType.MONTHLY:
+            start = date(today.year, today.month, 1)
+            _, num_days = calendar.monthrange(today.year, today.month)
+            end = date(today.year, today.month, num_days)
+        elif recurring_task.repetitive_type == RecurrenceType.DAILY:
+            start = today
+            end = today
+
+    # find if task history is found with existing data
+    task_history = session.exec(
+        select(TaskHistory).where(
+            TaskHistory.task_id == task.id,
+            TaskHistory.start == start,
+            TaskHistory.end == end,
+        )
+    ).first()
+
+    if task_history:
+        task_history.completed_at = today if status == TaskStatus.COMPLETED else None
+        session.commit()
+    else:
+        if status == TaskStatus.COMPLETED:
+            task_history = TaskHistory(
+                task_id=task.id, start=start, end=end, completed_at=today
+            )
+        else:
+            task_history = TaskHistory(task_id=task.id, start=start, end=end)
+        add_to_db(task_history, session)
+
+    session.refresh(task)  # updates the in-place relationships
+    return
+
+
 @router.get("/{id}", response_model=TaskOut)
 def get_task(
     id: int,
@@ -71,7 +151,7 @@ def get_task(
 def add_task(
     task_in: TaskIn,
     repetitive_type: Optional[RecurrenceType] = None,
-    repeat_until: Optional[datetime] = None,
+    repeat_until: Optional[date] = None,
     current_user=Depends(get_current_user),
     session: Session = Depends(get_session),
 ):
@@ -81,15 +161,18 @@ def add_task(
     task_data = task_in.model_dump(exclude_unset=True)
     task_data["user_id"] = user.id
 
-    task_data["pinned"] = task_data.get("pinned", False) and check_pinned_tasks(user.id, session)
+    task_data["pinned"] = task_data.get("pinned", False) and check_pinned_tasks(
+        user.id, session
+    )
 
     task = add_to_db(Task(**task_data), session)
     if repetitive_type or repeat_until:
         recurring_task = RecurringTask(
             task_id=task.id, repetitive_type=repetitive_type, repeat_until=repeat_until
         )
-        task.recurring_task = recurring_task
         add_to_db(recurring_task, session)
+        session.refresh(task)  # updates the in-place relationships
+
     return bind_task_details(task)
 
 
@@ -98,7 +181,7 @@ def update_task(
     id: int,
     task_in: TaskIn,
     repetitive_type: Optional[RecurrenceType] = None,
-    repeat_until: Optional[datetime] = None,
+    repeat_until: Optional[date] = None,
     remove_recurring: Optional[bool] = False,
     current_user=Depends(get_current_user),
     session: Session = Depends(get_session),
@@ -114,6 +197,10 @@ def update_task(
             value = task_data["pinned"] and check_pinned_tasks(task.user_id, session)
         if key == "status" and value:
             value = TaskStatus(value)
+        if task.recurring_task and key == "status" and value == TaskStatus.COMPLETED:
+            updateTaskHistory(task, value, session)
+            continue
+        print(key, value)
         setattr(task, key, value)
 
     if remove_recurring:
@@ -136,9 +223,7 @@ def update_task(
             task.recurring_task = recurring_task
             session.add(recurring_task)
 
-    session.add(task)
-    session.commit()
-    session.refresh(task)
+    task = add_to_db(task, session)
     return bind_task_details(task)
 
 
@@ -155,3 +240,19 @@ def delete_task(
         session.commit()
         msg = "Task Deleted successfully"
     return {"message": msg}
+
+
+@router.get("/history/{id}", response_model=list[TaskHistory])
+def get_history(
+    id: int,
+    current_user=Depends(get_current_user),
+    session: Session = Depends(get_session),
+):
+    task = get_task_by_id(id, session)
+    if verify_current_user(task.user_id, current_user["user_email"], session):
+        return task.task_history
+
+
+# @router.get("/recurring_tasks")
+# def recurring_tasks(session: Session = Depends(get_session)):
+#     return session.exec(select(RecurringTask)).all()
