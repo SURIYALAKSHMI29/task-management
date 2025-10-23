23c938204b761ce5aa531bf60ee9aaf53ad4b9df
SuriyaLakshmi29 <suriyalakshmi413@gmail.com>
2025-10-23 15:45:38 +0530

    Add TaskHistory table and indexes

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import List, Optional
 
 from pydantic import EmailStr
-from sqlmodel import Field, Relationship, SQLModel
+from sqlmodel import Field, Index, Relationship, SQLModel
 
 from backend.helpers.enums import RecurrenceType, TaskPriority, TaskStatus
 
@@ -16,6 +16,7 @@ class User(SQLModel, table=True):
     tasks: List["Task"] = Relationship(
         back_populates="user", sa_relationship_kwargs={"cascade": "all, delete-orphan"}
     )
+    __table_args__ = (Index("idx_email", "email"),)
 
 
 class Task(SQLModel, table=True):
@@ -33,6 +34,15 @@ class Task(SQLModel, table=True):
     recurring_task: Optional["RecurringTask"] = Relationship(
         back_populates="task", sa_relationship_kwargs={"cascade": "all, delete-orphan"}
     )
+    task_history: List["TaskHistory"] = Relationship(
+        back_populates="task", sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+    )
+
+    __table_args__ = (
+        Index("idx_user_id", "user_id"),
+        Index("idx_pinned", "pinned"),
+        Index("idx_status", "status"),
+    )
 
 
 class RecurringTask(SQLModel, table=True):
@@ -41,3 +51,15 @@ class RecurringTask(SQLModel, table=True):
     repetitive_type: RecurrenceType | None = Field(default=RecurrenceType.MONTHLY)
     repeat_until: datetime | None = Field(default=None)
     task: "Task" = Relationship(back_populates="recurring_task")
+    __table_args__ = (Index("idx_recurring_task_task_id", "task_id"),)
+
+
+class TaskHistory(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    task_id: int = Field(foreign_key="task.id")
+    start: datetime
+    end: datetime
+    completed_at: Optional[datetime] = None
+    task: "Task" = Relationship(back_populates="task_history")
+
+    __table_args__ = (Index("idx_task_history_task_id", "task_id"),)
