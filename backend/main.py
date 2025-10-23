3fe6d411804d5675c4541dcf0c9f29f611382fa0
SuriyaLakshmi29 <suriyalakshmi413@gmail.com>
2025-10-23 22:14:49 +0530

    Add scheduled job to update task history

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,27 @@
+from datetime import datetime
+
+from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi import FastAPI
 
 from backend.database import create_db_and_tables
 from backend.routers import tasks, users
+from backend.routers.tasks import scheduled_task_updates
 
 app = FastAPI()
+
+scheduler = BackgroundScheduler()
 
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+    # runs for every 24 hours
+    # if server is down and restarted, starts fresh
+    scheduler.add_job(
+        scheduled_task_updates, "interval", days=1, next_run_time=datetime.now()
+    )
+    scheduler.start()
 
 
 app.include_router(users.router, prefix="/user", tags=["user"])
