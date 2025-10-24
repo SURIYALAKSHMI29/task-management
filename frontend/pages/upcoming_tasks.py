9199e10c467a335eea195948df54e9542156a9a4
SuriyaLakshmi29 <suriyalakshmi413@gmail.com>
2025-10-24 17:42:25 +0530

    Modify calendar view and add deadline field in edit task modal

--- frontend/pages/upcoming_tasks.py
+++ frontend/pages/upcoming_tasks.py
@@ -4,7 +4,8 @@ from datetime import date, datetime
 import streamlit as st
 from streamlit_calendar import calendar
 from streamlit_modal import Modal
-from styles.home_css import inject_css
+
+from frontend.styles.task_css import inject_css
 
 inject_css()
 
@@ -18,10 +19,34 @@ upcoming_tasks = st.session_state.upcoming_tasks
 task_modal = Modal("Task Details", key="task-modal", padding=20, max_width=900)
 
 
+st.markdown(
+    """
+    <style>
+        div[data-modal-container="true"] > div > div {
+            width: 900px !important;      
+            min-width: 700px !important;   
+            max-width: 90vw !important;  
+        }
+        div[data-modal-container="true"] > div > div >div{
+            min-width: 700px !important;      
+        }
+
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
 def close_modal(task_modal: Modal):
     st.session_state["edit_task_modal_open"] = False
     task_modal.close()
     st.rerun()
+
+
+def get_isoformat_date(inp_date):
+    if type(inp_date) != date:
+        inp_date = datetime.strptime(inp_date, "%Y-%m-%d").date()
+    return inp_date.isoformat()
 
 
 def show_task(task_modal: Modal):
@@ -43,6 +68,8 @@ def show_task(task_modal: Modal):
 
     pinned = st.checkbox("Pinned", value=extended_props.get("pinned"))
 
+    deadline = st.date_input("Deadline", value=extended_props.get("deadline"))
+
     if st.button("Save Changes"):
         if st.session_state["edit_task"].get("id") == None:
             task = {
@@ -51,7 +78,7 @@ def show_task(task_modal: Modal):
                 "priority": priority,
                 "status": status,
                 "pinned": pinned,
-                "deadline": task.get("deadline"),
+                "deadline": deadline,
             }
             st.session_state["user_tasks"].append(task)
             print("\nAfter adding task:", st.session_state["user_tasks"], "\n")
@@ -64,6 +91,7 @@ def show_task(task_modal: Modal):
                     task["priority"] = priority
                     task["status"] = status
                     task["pinned"] = pinned
+                    task["deadline"] = deadline
                     break
             st.success("Task updated successfully.")
         time.sleep(2)
@@ -75,13 +103,14 @@ def show_task(task_modal: Modal):
 events = []
 print("\nUpcoming tasks:", upcoming_tasks, "\n\n")
 for task in upcoming_tasks:
+    deadline = get_isoformat_date(task["end"])
+
     events.append(
         {
             "id": str(task["id"]),
             "allday": (task["repetitive_type"] == "daily"),
             "title": task["title"],
-            "start": task["start"],
-            "end": task["end"],
+            "start": deadline,
             "color": (
                 "#990000"
                 if task.get("priority") == "high"
@@ -92,6 +121,7 @@ for task in upcoming_tasks:
                 "priority": task.get("priority"),
                 "status": task.get("status"),
                 "pinned": task.get("pinned"),
+                "deadline": deadline,
             },
         }
     )
@@ -106,6 +136,12 @@ calendar_options = {
     "initialView": "dayGridMonth",
     "editable": True,
     "selectable": False,
+    "eventDisplay": "list-item",
+    "buttonText": {
+        "dayGridMonth": "Month",
+        "listWeek": "Week List",
+        "listMonth": "Month List",
+    },
 }
 
 state = calendar(
@@ -120,7 +156,6 @@ if state.get("callback") == "eventClick":
     print(st.session_state["edit_task"])
     st.session_state["edit_task_modal_open"] = True
     task_modal.open()
-    print("here")
 
 if state.get("callback") == "dateClick":
     print("Date clicked")
