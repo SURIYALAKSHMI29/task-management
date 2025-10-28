09d0beb24b4abfd49d658305b8249211c1ded742
SuriyaLakshmi29 <suriyalakshmi413@gmail.com>
2025-10-28 14:10:29 +0530

    Modify display task param

--- frontend/pages/completed_tasks.py
+++ frontend/pages/completed_tasks.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
 from frontend.styles.task_css import inject_css
-from frontend.utils.task_util import display_tasks
+from frontend.utils.task_card import display_tasks
 
 inject_css()
 
 st.header("Completed Tasks")
 
-icon = "\N{HEAVY CHECK MARK}"
-
 completed_tasks = st.session_state.completed_tasks
-display_tasks(completed_tasks, completed=True, icon=icon)
+display_tasks(completed_tasks, completed=True)
