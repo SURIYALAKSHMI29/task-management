12bf981830e15e9358bc91be06661d3110b35b98
SuriyaLakshmi29 <suriyalakshmi413@gmail.com>
2025-10-31 14:59:22 +0530

    Modify nav-bar

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-
-from frontend.styles.app_css import inject_css
-from frontend.utils.initialize_sessions import initialize_sessions
+from styles.app_css import inject_css
+from utils.initialize_sessions import initialize_sessions
 
 inject_css()
 
@@ -9,18 +8,31 @@ st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
 initialize_sessions()
 
-home_page = st.Page("pages/home.py", title="GoGetter", icon=":material/home:")
-tasks_page = st.Page("pages/tasks.py", title="Tasks", icon=":material/hourglass_top:")
-profile_page = st.Page(
-    "pages/profile.py", title="Profile", icon=":material/account_circle:"
+home_page = st.Page("pages/home.py", title="Home", icon=":material/home:")
+tasks_page = st.Page(
+    "pages/tasks.py", title="All Tasks", icon=":material/hourglass_top:"
 )
 upcoming_tasks_page = st.Page(
-    "pages/upcoming_tasks.py", title="Upcoming tasks", icon=":material/schedule:"
+    "pages/upcoming_tasks.py", title="Upcoming", icon=":material/schedule:"
 )
 completed_tasks_page = st.Page(
-    "pages/completed_tasks.py", title="Completed tasks", icon=":material/check_circle:"
+    "pages/completed_tasks.py", title="Completed", icon=":material/check_circle:"
 )
 
-pg = st.navigation([home_page, tasks_page, upcoming_tasks_page, completed_tasks_page])
+workspace_page = st.Page(
+    "pages/workspace.py", title="Workspace", icon=":material/workspaces:"
+)
+
+groups_page = st.Page("pages/groups.py", title="Task Groups", icon=":material/layers:")
+
+pg = st.navigation(
+    {
+        "GoGetter": [
+            home_page,
+        ],
+        "My Workspace": [workspace_page],
+        "Tasks": [tasks_page, groups_page, upcoming_tasks_page, completed_tasks_page],
+    }
+)
 
 pg.run()
